c667fc3fb5cf65f0798a4a7789aaa3912b4901ca
adcpm <fabien@adcpm.com>
2016-07-26 04:55:21 +0700

    - Add gitignore

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var get = require('get-parameter-names');
+var decamelize = require('decamelize');
 var Steem = require('steem'),
   steem = new Steem();
 
 router.get('/:method', function(req, res, next) {
-  var params = req.query;
-  steem[req.params.method](function(err, result) {
+  var query = req.query;
+  var lenght = steem[req.params.method].length;
+  var options = get(steem[req.params.method]);
+  var callback = function(err, result) {
+    res.json(result);
+  };
+  var params = [];
+  options.forEach(function(option) {
+    if (query[option]) {
+      params.push(query[option]);
+    }
+  });
+  var method = decamelize(req.params.method, '_');
+  var data = {
+    method: method,
+    params: params
+  };
+  steem.send('database_api', data, function(err, result) {
     res.json(result);
   });
 });
