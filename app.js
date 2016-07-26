b5ca382522b90eea0aa0856c28766092a16ba5cb
adcpm <fabien@adcpm.com>
2016-07-26 07:54:58 +0700

    - Restructure

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
-app.use('/api', require('./routes/api'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var get = require('get-parameter-names');
+var decamelize = require('decamelize');
+var Steem = require('steem'),
+  steem = new Steem();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:method', function(req, res, next) {
+  var query = req.query;
+  var lenght = steem[req.params.method].length;
+  var options = get(steem[req.params.method]);
+  var callback = function(err, result) {
+    res.json(result);
+  };
+  var params = [];
+  options.forEach(function(option) {
+    if (query[option]) {
+      params.push(query[option]);
+    }
+  });
+  var method = decamelize(req.params.method, '_');
+  var data = {
+    method: method,
+    params: params
+  };
+  steem.send('database_api', data, function(err, result) {
+    res.json(result);
+  });
 });
 
 module.exports = router;
