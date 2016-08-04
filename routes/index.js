e5030a286009eb13da4b44eeac6ee34977d49deb
adcpm <fabien@adcpm.com>
2016-08-04 11:27:55 +0700

    - Fix bug

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/@:name/friends', function(req, res, next) {
   var steem = new Steem();
   var name = req.params.name;
   steem.getFollowing(name, 0, 10, function(err, result) {
+    if (!err) {
      var count = result.length;
      var done = 0;
      var content = [];
@@ -21,6 +22,7 @@ router.get('/@:name/friends', function(req, res, next) {
          }
        });
      }
+    }
   });
 });
 
