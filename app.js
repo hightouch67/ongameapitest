3645a7ca247812e32d2f6bbf001d9a4b8d048f4d
adcpm <fabien@bonustrack.co>
2016-12-13 19:04:20 +0100

    Remove dep "cookie-parser", "moment" and "node-sass-middleware"

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var http = require('http'),
@@ -14,54 +13,28 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
-}));
 app.use(cors());
 
 // view engine setup
 var hbs = require('hbs');
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public'), {maxAge: (86400000 * 7)}));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', require('./routes/api'));
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json(['error', {
     message: err.message,
     error: {}
-  });
+  }]);
 });
 
 
