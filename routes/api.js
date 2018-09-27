092b5b576b7dccd8cdb737b7ed001b7597638c62
hightouch <hightouch@hotmail.fr>
2018-09-27 05:36:00 +0200

    update

--- routes/api.js
+++ routes/api.js
@@ -102,7 +102,7 @@ var connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
-  database: MYSQL_DB
+  database: process.env.MYSQL_DB
 });
 
 connection.connect();
