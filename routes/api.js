1bece6a3415bf6249b9ca887975c4d24354e4f4a
hightouch <hightouch@hotmail.fr>
2018-09-27 03:23:42 +0200

    update

--- routes/api.js
+++ routes/api.js
@@ -17,61 +17,133 @@ function handleError(res, reason, message, code) {
 }
 
 router.get("/api/characters", function (req, res) {
-  var element = {} ,characters = []
+  var element = {}, characters = []
   var query = "SELECT * FROM user"
-  Connect(query,element,function(error){
-    if(error)
-    res.json(error)
+  Connect(query, element, function (result) {
+    if (result)
+      res.json(result)
   })
 });
 
-router.get("/api/character/:id", function (req, res) {
-  var element = {} ,characters = []
-  var query = "SELECT * FROM characters WHERE character_id="+ req.params.id
-  Connect(query,element,function(error){
-    if(error)
-    res.json(error)
-  })
-});
-
-
-Connect = function (query,object, cb) {
-  var con = mysql.createConnection({
-    host: "db4free.net",
-    user: "ongame",
-    password: "Abcdef55",
-    database: "ongame"
-  })
-    var element = {} ,characters = []
-  con.connect(function (err) {
-    if (err) throw err;
-    // var json = '{"skin": "none","hr_weapon": "none","hl_weapon": "none", "body": "none","bottom": "none","hat": "none"}'
-    // var id = getHash('hightouch')
-    // username = "hightouch"
-    // //var query = "INSERT INTO users (id, name, level, xp, inventory) VALUES ('"+id +"," +username +"','"+json+"') ON DUPLICATE KEY UPDATE id = id + 1"
-    // var query = "SELECT * FROM user"
-    // console.log("Connected!");
-    // var sql = "INSERT INTO users (name, inventory) VALUES ('hightouch','[name1]')";
-
-    con.query(query, function (err, result) {
-      if (err) return cb(null);
-      for (var i = 0; i < result.length; i++) {
-        element = result[i]
-        var id = element.name
-        characters.push(element)
-      }
-      return cb(characters)
+router.get("/api/character/:name", function (req, res) {
+  var playerid;
+  var character = {}
+  //LOAD CHARACTER
+  var query = "SELECT * FROM user WHERE first_name='" + req.params.name + "'"
+  Connect(query, function (result) {
+    if (result)
+      playerid = result[0].user_id
+    character.character = result[0]
+    //LOAD EQUIPMENT
+    var query = "SELECT * FROM characters WHERE character_id='" + playerid + "'"
+    Connect(query, function (result) {
+      if (result)
+        character.equipment = result[0]
+      //LOAD ATTRIBUTES
+      var query = "SELECT * FROM character_attribute WHERE character_id='" + playerid + "'"
+      Connect(query, function (result) {
+        if (result)
+          character.attributes = result
+        //LOAD ITEMS
+        var query = "SELECT * FROM character_item WHERE character_id='" + playerid + "'"
+        Connect(query, function (result) {
+          if (result)
+            character.items = result
+          res.json(character)
+        })
+      })
     })
   })
+});
+
+router.get("/api/properties", function (req, res) {
+  var properties = {}
+  //LOAD ATTRIBUTES
+  var query = "SELECT * FROM attribute"
+  Connect(query, function (result) {
+    if (result)
+      properties.attributes = result
+    //LOAD ITEMS
+    var query = "SELECT * FROM item"
+    Connect(query, function (result) {
+      if (result)
+        properties.items = result
+      //LOAD ITEMS ATTRIBUTES
+      var query = "SELECT * FROM item_attribute"
+      Connect(query, function (result) {
+        if (result)
+          properties.items_attributes = result
+        //LOAD ITEMS TYPES
+        var query = "SELECT * FROM item_type"
+        Connect(query, function (result) {
+          if (result)
+            properties.items_types = result
+          //LOAD SLOTS
+          var query = "SELECT * FROM equipment_slot"
+          Connect(query, function (result) {
+            if (result)
+              properties.slots = result
+            res.json(properties)
+          })
+        })
+      })
+    })
+  })
+});
+// Connect = function (query, object, cb) {
+//   var con = mysql.createConnection({
+//     host: "db4free.net",
+//     user: "ongame",
+//     password: "Abcdef55",
+//     database: "ongame"
+//   })
+//   var element = {}, characters = []
+//   con.connect(function (err) {
+//     if (err) throw err;
+//     con.query(query, function (err, result) {
+//       if (err) return cb(null);
+//       for (var i = 0; i < result.length; i++) {
+//         element = result[i]
+//         characters.push(element)
+//       }
+//       return cb(characters)
+//     })
+//   })
+
+// }
+
+var connection = mysql.createConnection({
+  host: 'db4free.net',
+  user: 'ongame',
+  password: 'Abcdef55',
+  database: 'ongame'
+});
+
+connection.connect();
+
+
+Connect = function (query, cb) {
+  connection.query(query, function (error, results, fields) {
+    if (error) throw error;
+    else {
+      return cb(results)
+    }
+  });
 
 }
+
+
+
+
+
 function getHash(input) {
   var hash = 0, len = input.length;
   for (var i = 0; i < len; i++) {
-      hash = ((hash << 5) - hash) + input.charCodeAt(i);
-      hash |= 0; // to 32bit integer
+    hash = ((hash << 5) - hash) + input.charCodeAt(i);
+    hash |= 0; // to 32bit integer
   }
   return hash;
+  connection.end();
 }
 
 module.exports = router;
