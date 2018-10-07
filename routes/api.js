dd31badb833272e7c63e13916cee9616a3f169d2
hightouch <hightouch@hotmail.fr>
2018-10-07 16:14:03 +0200

    sql

--- routes/api.js
+++ routes/api.js
@@ -69,7 +69,7 @@ loadSingle = function (author, permlink, cb) {
   steem.api.getContent(author, permlink, function (error, result) {
     if (result) {
       try {
-        result.json_metadata = JSON.parse(result.json_metadata)
+        result.json_metadata = JSON.stringify(result.json_metadata)
       } catch (e) {
       }
       cb(result)
