50558d7fdb6ee09fe35ab23c5720e6fb888be592
hightouch <hightouch@hotmail.fr>
2018-10-07 14:38:20 +0200

    sql

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,10 @@ const _ = require('lodash');
 var mysql = require('mysql');
 const router = express.Router();
 const sql = require('mssql')
+const steem = require('@steemit/steem-js');
+
+
+
 router.get('/', (req, res) => {
   res.json({ hello: 'world' });
 });
@@ -22,34 +26,10 @@ var pool1 = mysql.createPool({
   database: process.env.MYSQL_DB
 });
 
-const config = {
-  user: process.env.SQL_USERNAME,
-  password: process.env.SQL_PASSWORD,
-  server: process.env.SQL_HOST,
-  database: process.env.SQL_DB,
-  pool: {
-    max: 10,
-    min: 0,
-    idleTimeoutMillis: 30000
-  },
-  options: {
-    encrypt: true // Use this if you're on Windows Azure
-  }
-}
-
-const pool = new sql.ConnectionPool({
-  user: process.env.SQL_USERNAME,
-  password: process.env.SQL_PASSWORD,
-  server: process.env.SQL_HOST,
-  database: process.env.SQL_DB
-})
-
-
-
 
 router.get("/api/user/:name/:permlink", function (req, res) {
   pool1.getConnection(function (error, connection) {
-    var query = `SELECT * FROM comments where author='${req.params.name}' AND permlink='${req.params.permlink}'`
+    var query = `SELECT * FROM projects where author='${req.params.name}' AND permlink='${req.params.permlink}'`
     connection.query(query, function (err, result) {
       if (err) return;
       else
@@ -59,138 +39,48 @@ router.get("/api/user/:name/:permlink", function (req, res) {
   })
 })
 
-  router.get("/api/fullprojects", function (req, res) {
-    pool1.getConnection(function (error, connection) {
-      var query = `SELECT * FROM comments`
-      connection.query(query, function (err, result) {
-        if (err) return;
-        else
-          res.json(result)
-        connection.release();
-      })
+router.get("/api/fullprojects", function (req, res) {
+  pool1.getConnection(function (error, connection) {
+    var query = `SELECT * FROM projects`
+    connection.query(query, function (err, result) {
+      if (err) return;
+      else
+        res.json(result)
+      connection.release();
     })
   })
+})
 
-  router.get("/api/projects", function (req, res) {
-    pool1.getConnection(function (error, connection) {
-      var query = `SELECT author, permlink, title, json_metadata, type FROM comments`
-      connection.query(query, function (err, result) {
-        if (err) return;
-        else
-          res.json(result)
-        connection.release();
-      })
+router.get("/api/projects", function (req, res) {
+  pool1.getConnection(function (error, connection) {
+    var query = `SELECT author, permlink, title, json_metadata, type FROM projects`
+    connection.query(query, function (err, result) {
+      if (err) return;
+      else
+        res.json(result)
+      connection.release();
     })
   })
+})
 
-  // var query = `select *
-  // from
-  //     Comments
-  // where   
-  //      author='${req.params.name}'
-  //     AND        
-  //         (    
-  //         (ISJSON(json_metadata) > 0) and
-  //         (
-  //              ( JSON_VALUE(json_metadata,'$.tags[5]') in ('${req.params.permlink}') ) or  
-  //              ( JSON_VALUE(json_metadata,'$.tags[4]') in ('${req.params.permlink}') ) or   
-  //              ( JSON_VALUE(json_metadata,'$.tags[3]') in ('${req.params.permlink}') ) or   
-  //              ( JSON_VALUE(json_metadata,'$.tags[2]') in ('${req.params.permlink}') ) or   
-  //              ( JSON_VALUE(json_metadata,'$.tags[1]') in ('${req.params.permlink}') ) or   
-  //              ( JSON_VALUE(json_metadata,'$.tags[0]') in ('${req.params.permlink}') )
-  //         )
-  //       )
-  // order by 
-  //     created desc`
-  // execute2(query)
-  // async function execute2(query) {
+router.get("/api/add/:name/:permlink", function (req, res) {
+  steem.api.getContent(req.params.name, req.params.permlink, function(err, result) {
+    console.log(err, result);
+  });
+  // pool1.getConnection(function (error, connection) {
 
-  //   return new Promise((resolve, reject) => {
-  
-  //     new sql.ConnectionPool(config).connect().then(pool => {
-  //       return pool.request().query(query)
-  //     }).then(result => {
-  //       res.json( result.recordset)
-  //       resolve(result.recordset);
-  //       sql.close();
-  //     }).catch(err => {
-  //       reject(err)
-  //       sql.close();
-  //     });
-  //   });
-  // }
-  
-// new sql.ConnectionPool(config).connect().then(pool => {
-//   return pool.request().query(`select * from Comments where author = '${req.params.name}'`)
-//   }).then(result => {
-//     let rows = result.recordset
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.status(200).json(rows);
-//     sql.close();
-//   }).catch(err => {
-//     res.status(500).send({ message: "${err}"})
-//     sql.close();
-//   });
 
-//   var name = req.params.name
-//   (async function () {
-//     try {
-//         let pool = await sql.connect(config)
-//         let result1 = await pool.request()
-//             .input('input_parameter', sql.Int, value)
-//             .query(`select * from Comments where author = '${name}'`)
-
-//         console.dir(result1)
-//         sql.close()
-//     } catch (err) {
-//         // ... error checks
-//         sql.close()
-//     }
-// })()
-
-// sql.on('error', err => {
-//     // ... error handler
-//     sql.close()
-// })
-
-// var array = []
-// var test = "hightouch"
-// name = req.params.name
-// sql.connect(config, err => {
-//   // ... error checks
-
-//   var request = new sql.Request()
-//   request.stream = true // You can set streaming differently for each request
-//   request.query(`select * from Comments where author = '${test}'`)  
-//   request.on('recordset', columns => {
-//       console.log(columns)
-//       // Emitted once for each recordset in a query
-//   })
-
-//   request.on('row', row => {
-//       console.log(row)
-//       array.push(row)
-//       // Emitted for each row in a recordset
-//   })
-
-//   request.on('error', err => {
-//       // May be emitted multiple times
-//       sql.close();
-//   })
-
-//   request.on('done', result => {
-//       // Always emitted as the last one
-//       console.log(result)
-//       res.json(array)
-//       sql.close();
-//   })
-// })
-
-// sql.on('error', err => {
-//   // ... error handler
-//   sql.close();
-// })
-
+  //   var query = `INSERT INTO * FROM projects where 
+  //                                 author='${req.params.name}' AND 
+  //                                 permlink='${req.params.permlink}'`
+  //   connection.query(query, function (err, result) {
+  //     if (err) return;
+  //     else
+  //       res.json(result)
+  //     connection.release();
+  //   })
+  // })
+})
 
 
 router.get("/api/characters", function (req, res) {
