0030a5c1cd0c303e7460a6601a3ede1c303a1e0d
hightouch <hightouch@hotmail.fr>
2018-10-08 15:39:19 +0200

    update

--- routes/api.js
+++ routes/api.js
@@ -68,10 +68,10 @@ router.get("/api/projects", function (req, res) {
 loadSingle = function (author, permlink, cb) {
   steem.api.getContent(author, permlink, function (error, result) {
     if (result) {
-      try {
-        result.json_metadata = JSON.stringify(result.json_metadata)
-      } catch (e) {
-      }
+      // try {
+      //   result.json_metadata = JSON.stringify(result.json_metadata)
+      // } catch (e) {
+      // }
       // result.rewards = JSON.stringify(result.json_metadata.rewards)
       // result.goals = JSON.stringify(result.json_metadata.goals)
       // result.image = setImage(result.json_metadata.basics.description)
